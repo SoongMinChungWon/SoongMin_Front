8e921f72dd63d72abde5660c86059e45944d89e5
kuj7373 <kuj7373@naver.com>
2024-07-27 11:12:48 +0900

    마이페이지 구현 + 연결

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,11 +1,13 @@
 import 'package:go_router/go_router.dart';
 
 import 'package:flutter/material.dart';
+import 'package:sw/src/MyPage.dart';
 import 'package:sw/src/bottom_nav.dart';
 import 'package:sw/src/LoginMian.dart';
 import 'package:sw/src/Main_Screen.dart';
 import 'package:sw/src/home.dart';
 import 'package:sw/src/wait.dart';
+
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 final ValueNotifier<bool> refreshNotifier = ValueNotifier<bool>(false);
@@ -31,6 +33,10 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/wait',
       builder: (context, state) => Wait(),
+    ),
+    GoRoute(
+      path: '/mypage',
+      builder: (context, state) => MyPage(),
     ),
   ],
 );
