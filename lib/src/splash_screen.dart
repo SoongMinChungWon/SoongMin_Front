b96a93f2a7022eba621687bc6243aa2acdb47904
kimjunha <tto9011@naver.com>
2024-08-02 02:47:37 +0900

    adss

--- lib/src/splash_screen.dart
+++ lib/src/splash_screen.dart
@@ -2,11 +2,8 @@ import 'dart:async';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'package:sw/core/routes/routes.dart';
-
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    requestPermissions();
+
     // 3초 후에 LoginMain 페이지로 이동
     Timer(Duration(seconds: 3), () {
       refreshNotifier.value = !refreshNotifier.value; // 상태 업데이트
@@ -28,25 +25,34 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
-  Future<void> requestPermissions() async {
-    await [Permission.camera, Permission.photos].request();
-  }
-
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splashIconSize: 162.w, // 크기를 screenutil로 조정
+      backgroundColor: Color(0xff87ceeb),
+      splashIconSize: MediaQuery.sizeOf(context).width,
       duration: 3000,
-      splash: Container(
-        // margin:
-        //     EdgeInsets.symmetric(horizontal: 114.w), // 좌우 여백을 screenutil로 비율 조정
-        width: 162.w, // 너비를 screenutil로 비율 조정
-        height: 127.w,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/splashs.png'),
-            fit: BoxFit.fitWidth, // 이미지를 중앙에 고정하고 비율을 유지하며 맞춤
-          ),
+      splash: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_balance,
+              size: 50,
+              color: Colors.white,
+            ),
+            SizedBox(height: 20),
+            Text(
+              '숭민청원',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
       nextScreen: Container(), // 임시로 Placeholder 사용
